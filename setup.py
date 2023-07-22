82196670cfc8edd29156513abf1f01499514fb6f
hsiao-tung tan <reyes.tan@foxmail.com>
2023-07-22 08:02:37 +0800

    initial

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+packages = find_packages()
+packages.append('scripts')
+
+setup(
+    name='pl_hsir',
+    description="Hyperspectral Image Restoration Tools",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=packages,
+    package_dir={'hst_engine': 'hst_engine', 'scripts': 'scripts'},
+    version='0.0.1',
+    include_package_data=True,
+    install_requires=['tensorboard', 'torch', 'torchvision', 'numpy', 'lightning', 'timm', 'einops'],
+)
